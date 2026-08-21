f3aa311104a77275ffc892e0bf9bfe6e062c72a0
Harsh <harsh2021800@gmail.com>
2026-08-21 19:49:43 +0530

    Delete frontend directory

--- frontend/src/pages/AboutPage.tsx
+++
@@ -1,9 +0,0 @@
-import { About } from '@/components/About';
-
-export function AboutPage() {
-  return (
-    <div className="pt-20">
-      <About />
-    </div>
-  );
-}
--- frontend/src/pages/ResumePage.tsx
+++
@@ -1,9 +0,0 @@
-import { Resume } from '@/components/Resume';
-
-export function ResumePage() {
-  return (
-    <div className="pt-20">
-      <Resume />
-    </div>
-  );
-}
